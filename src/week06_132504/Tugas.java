5ff931ebf2d436ab1b0319cf317c862cf9ac3961
azizandri <azizandri72@gmail.com>
2025-10-01 11:04:56 +0700

    commit pertama

---
+++ src/week06_132504/Tugas.java
@@ -0,0 +1,17 @@
+package week06_132504;
+
+import java.util.Scanner;
+
+public class Tugas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("----- Menu Toko Vocher & HP -----");
+		System.out.println("1. Pesan Barang ");
+		System.out.println("2. Lihat Pesanan ");
+		System.out.println("3. Barang Baru ");
+		System.out.println("Pilihan : ");
+		Scanner sc = new Scanner (System.in);
+	}
+
+}
